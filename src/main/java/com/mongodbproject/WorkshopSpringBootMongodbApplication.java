0ca5ae3912891b1767d9689092bb82caa0c1bffe
Zaine Mendes <zainemendes@hotmail.com>
2024-10-31 14:10:54 -0300

    criação do projeto

---
+++ src/main/java/com/mongodbproject/WorkshopSpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.mongodbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mongodbproject/WorkshopSpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongodbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
